ac8fef2d493abcb90e5ae4c791a78e9c4ef806b7
thanasinb <thanasin.b@gmail.com>
2024-09-28 11:26:51 +0700

    fix path

--- examples/2DNoisyXORDemo.py
+++ examples/2DNoisyXORDemo.py
@@ -1,13 +1,14 @@
 #!/usr/local/bin/python3
-
+import os
 from pyTsetlinMachine.tm import MultiClassConvolutionalTsetlinMachine2D
 import numpy as np 
 
-train_data = np.loadtxt("2DNoisyXORTrainingData.txt")
+this_dir, this_filename = os.path.split(__file__)
+train_data = np.loadtxt(os.path.join(this_dir, "2DNoisyXORTrainingData.txt"))
 X_train = train_data[:,0:-1].reshape(train_data.shape[0], 4, 4)
 Y_train = train_data[:,-1]
 
-test_data = np.loadtxt("2DNoisyXORTestData.txt")
+test_data = np.loadtxt(os.path.join(this_dir, "2DNoisyXORTestData.txt"))
 X_test = test_data[:,0:-1].reshape(test_data.shape[0], 4, 4)
 Y_test = test_data[:,-1]
 
@@ -24,4 +25,5 @@ Xi = np.array([[[0,1,1,0],
 
 print("\nInput Image:\n")
 print(Xi)
-print("\nPrediction: %d" % (ctm.predict(Xi)))
+# print("\nPrediction: %d" % (ctm.predict(Xi)))
+print("\nPrediction: %d" % ctm.predict(Xi)[0])
